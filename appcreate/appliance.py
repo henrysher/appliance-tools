dd758ef3cbe55aaf09fe2235f6caf6b3da0fd2aa
David Huff <dhuff@redhat.com>
2008-07-24 16:10:43 -0400

    fixed compat-shims-in-fs.py-for-old-api.patch

--- appcreate/appliance.py
+++ appcreate/appliance.py
@@ -69,7 +69,10 @@ class ApplianceImageCreator(ImageCreator):
                 'mountpoint': p['mountpoint'],
                 'fstype': p['fstype'] }
 
-        s += self._get_fstab_special()
+        s += "devpts     /dev/pts  devpts  gid=5,mode=620   0 0\n"
+        s += "tmpfs      /dev/shm  tmpfs   defaults         0 0\n"
+        s += "proc       /proc     proc    defaults         0 0\n"
+        s += "sysfs      /sys      sysfs   defaults         0 0\n"
         return s
     
     
